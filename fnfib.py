4bac662c1f43ec49a929399a5e5a42d45412bb1a
hamnabasheer <145666882+hamnabasheer@users.noreply.github.com>
2023-10-27 16:02:58 +0530

    Add files via upload

---
+++ fnfib.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    f1=0
+    f2=1
+    print(f1, end=" ")
+    print(f2, end=" ")
+    f3=f1+f2
+    while(f3<=n):
+        print(f3, end=" ")
+        f1=f2
+        f2=f3
+        f3=f1+f2
+i=int(input("enter  the nmbr: "))
+fibonacci(i)
