740505ad869e4a12181132bc12e0c7936fbe94e7
hamnabasheer <145666882+hamnabasheer@users.noreply.github.com>
2023-10-27 16:28:13 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,8 @@
+a=lambda a:a*a
+print("area of square with length 5 is ",a(5))
+x=lambda a,b:a*b
+print("area of square with length 5 is ",x(5,3))
+c=lambda a:a*a*2*3.14
+print("area of circle with radius 5 is ",c(5))
+t=lambda a,h:a*h/2
+print("area of triangle with height 5 and length 3 is ",t(5,3))
